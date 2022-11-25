e99db422a606d7182c01f6a8ba8c27e36882e461
aftermath <3368760963@qq.com>
2022-11-26 01:56:06 +0800

    style(utils): 改动部分UUID代码，消除部分警告

--- natu-common/src/main/java/com/nt/common/utils/uuid/UUID.java
+++ natu-common/src/main/java/com/nt/common/utils/uuid/UUID.java
@@ -385,11 +385,9 @@ public final class UUID implements java.io.Serializable, Comparable<UUID> {
     public int compareTo(UUID val) {
         // The ordering is intentionally set up so that the UUIDs
         // can simply be numerically compared as two numbers
-        return (this.mostSigBits < val.mostSigBits ? -1 : //
-                (this.mostSigBits > val.mostSigBits ? 1 : //
-                        (this.leastSigBits < val.leastSigBits ? -1 : //
-                                (this.leastSigBits > val.leastSigBits ? 1 : //
-                                        0))));
+        return (this.mostSigBits < val.mostSigBits ? -1 :
+                (this.mostSigBits > val.mostSigBits ? 1 :
+                        (Long.compare(this.leastSigBits, val.leastSigBits))));
     }
 
     // -------------------------------------------------------------------------------------------------------------------
